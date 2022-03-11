8a3ac84464b3ff9c140477cf6af61e7dbcfa5fd5
KevinJStricklin <kevin@lanmail.com>
2022-03-11 15:10:37 -0500

    Fix syntax errors

--- keyboards/annepro2/keymaps/stricklin/keymap.c
+++ keyboards/annepro2/keymaps/stricklin/keymap.c
@@ -16,18 +16,22 @@
 
 #include QMK_KEYBOARD_H
 
+#define KC_UNDO  LCTL(KC_Z)
+#define KC_CUT   LCTL(KC_X)
+#define KC_COPY  LCTL(KC_C)
+#define KC_PASTE LCTL(KC_V)
+
 enum anne_pro_layers {
-    _BASE_LAYER,
-    _FN1_LAYER,
-    _FN2_LAYER,
-    _MSE_LAYER
+    BASE,
+    FN1,
+    FN2,
+    MOUSE
 };
 
 // clang-format off
-
 // Key symbols are based on QMK. Use them to remap your keyboard
 /*
-* Layer _BASE_LAYER
+* Layer BASE
 * ,-----------------------------------------------------------------------------------------.
 * | esc |  1  |  2  |  3  |  4  |  5  |  6  |  7  |  8  |  9  |  0  |  -  |  =  |    Bksp   |
 * |-----------------------------------------------------------------------------------------+
@@ -39,7 +43,7 @@ enum anne_pro_layers {
 * |-----------------------------------------------------------------------------------------+
 * | Ctrl  |  L1   |  Alt  |               space             |  Alt  |  FN1  |  FN2  | Ctrl  |
 * \-----------------------------------------------------------------------------------------/
-* Layer TAP in _BASE_LAYER
+* Layer TAP in BASE
 * ,-----------------------------------------------------------------------------------------.
 * |     |     |     |     |     |     |     |     |     |     |     |     |     |           |
 * |-----------------------------------------------------------------------------------------+
@@ -52,24 +56,16 @@ enum anne_pro_layers {
 * |       |       |       |                                 |       |  LEFT | DOWN  | RIGHT |
 * \-----------------------------------------------------------------------------------------/
 */
-//  const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-//  [_BASE_LAYER] = LAYOUT( /* Base */
-//     KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC,
-//     KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS,
-//     LT(_FN1_LAYER,KC_CAPS), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,
-//     KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, RSFT_T(KC_UP),
-//     KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, KC_RALT, LT(_FN1_LAYER,KC_LEFT), LT(_FN2_LAYER,KC_DOWN), RCTL_T(KC_RGHT)
-// ),
  const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
- [_BASE_LAYER] = LAYOUT( /* Base */
-    KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC,
-    KC_TAB, KC_Q, KC_W, KC_F, KC_P, KC_G, KC_J, KC_L, KC_U, KC_Y, KC_SCLN, KC_LBRC, KC_RBRC, KC_BSLS,
-    LT(_FN1_LAYER,KC_CAPS), LGUI(KC_A), LALT(KC_R), LCTL(KC_S), LSFT(KC_T), KC_D, KC_H, RSFT(KC_N), RCTL(KC_E), RALT(KC_I), RGUI(KC_O), KC_QUOT, KC_ENT,
-    KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_K, KC_M, KC_COMM, KC_DOT, KC_SLSH, RSFT_T(KC_UP),
-    KC_LCTL, KC_LGUI, KC_LALT, LT(_FN1_LAYER,KC_SPC), MO(_MSE_LAYER), LT(_FN1_LAYER,KC_LEFT), LT(_FN2_LAYER,KC_DOWN), RCTL_T(KC_RGHT)
+ [BASE] = LAYOUT_60_ansi( /* Base */
+    KC_ESC,          KC_1,         KC_2, KC_3,   KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC,
+    KC_TAB,          KC_Q,         KC_W, KC_F,   KC_P, KC_G, KC_J, KC_L, KC_U, KC_Y, KC_SCLN, KC_LBRC, KC_RBRC, KC_BSLS,
+    LT(FN1,KC_CAPS), LGUI_T(KC_A), LALT_T(KC_R), LCTL_T(KC_S), LSFT_T(KC_T), KC_D, KC_H, RSFT_T(KC_N), RCTL_T(KC_E), RALT_T(KC_I), RGUI_T(KC_O), KC_QUOT, KC_ENT,
+    KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_K, KC_M, KC_COMM, KC_DOT, LT(MOUSE, KC_SLSH), RSFT_T(KC_UP),
+    KC_LCTL, KC_LGUI, KC_LALT, LT(FN1,KC_SPC), MO(MOUSE), LT(FN1,KC_LEFT), LT(FN2,KC_DOWN), RCTL_T(KC_RGHT)
 ),
   /*
-  * Layer _FN1_LAYER
+  * Layer FN1
   * ,-----------------------------------------------------------------------------------------.
   * |  `  |  F1 |  F2 |  F3 |  F4 |  F5 |  F6 |  F7 |  F8 |  F9 | F10 | F11 | F12 |  DELETE   |
   * |-----------------------------------------------------------------------------------------+
@@ -77,50 +73,49 @@ enum anne_pro_layers {
   * |-----------------------------------------------------------------------------------------+
   * | Esc     |LEFT |DOWN |RIGHT|  f  |  g  |  h  |  j  |  k  |  l  | PGUP|PGDN |    Enter    |
   * |-----------------------------------------------------------------------------------------+
-  * | Shift      |V-UP |V-DWN|MUTE |  v  |  b  |  n  |  m  |  ,  |INSRT| DEL |    Shift       |
-  * |-----------------------------------------------------------------------------------------+
   * | Ctrl  |  L1   |  Alt  |               space             |  Alt  |  FN1  |  FN2  | Ctrl  |
   * \-----------------------------------------------------------------------------------------/
   *
   */
- [_FN1_LAYER] = LAYOUT( /* Base */
+ [FN1] = LAYOUT_60_ansi( /* FN1 */
     KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,
     _______, KC_PGUP, KC_HOME, KC_UP,   KC_END,  _______, _______, _______, _______, _______, _______, KC_PSCR, KC_HOME, KC_END,
     _______, KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, KC_RSFT, KC_RCTL, KC_RALT, KC_RGUI, _______, _______,
     _______, KC_UNDO, KC_CUT,  KC_COPY, KC_PSTE, _______, _______, _______, _______, KC_INS, KC_DEL, _______,
-    _______, _______, _______, _______, _______, _______, MO(_FN2_LAYER), _______
+    _______, _______, _______, _______, _______, _______, MO(FN2), _______
 ),
   /*
-  * Layer _FN2_LAYER
+  * Layer FN2
   * ,-----------------------------------------------------------------------------------------.
-  * |  ~  | BT1 | BT2 | BT3 | BT4 |  F5 |  F6 |  F7 |LEDOF|LEDON| F10 | F11 | F12 |    Bksp   |
   * |-----------------------------------------------------------------------------------------+
-  * | Tab    |  q  | UP  |  e  |  r  |  t  |  y  |  u  |  i  |  o  | PS | HOME | END |   \    |
   * |-----------------------------------------------------------------------------------------+
-  * | Esc     |LEFT |DOWN |RIGHT|  f  |  g  |  h  |  j  |  k  |  l  | PGUP|PGDN |    Enter    |
   * |-----------------------------------------------------------------------------------------+
+  * |-----------------------------------------------------------------------------------------+
+  * | Shift      |  z  |  x  |  c  |  v  |  b  |  n  |  m  |  ,  |INSRT| DEL |    Shift       |
+  * | Shift      |  z  |  x  |  c  |  v  |  b  |  n  |  m  |  ,  |INSRT| DEL |    Shift       |
+  * | Shift      |  z  |  x  |  c  |  v  |  b  |  n  |  m  |  ,  |INSRT| DEL |    Shift       |
   * | Shift      |  z  |  x  |  c  |  v  |  b  |  n  |  m  |  ,  |INSRT| DEL |    Shift       |
   * |-----------------------------------------------------------------------------------------+
   * | Ctrl  |  L1   |  Alt  |               space             |  Alt  |  FN1  |  FN2  | Ctrl  |
   * \-----------------------------------------------------------------------------------------/
   *
   */
- [_FN2_LAYER] = LAYOUT( /* Base */
-    KC_TRNS, KC_AP2_BT1, KC_AP2_BT2, KC_AP2_BT3, KC_AP2_BT4, KC_TRNS, KC_TRNS, KC_TRNS, KC_AP_LED_OFF, KC_AP_LED_ON, KC_AP_LED_NEXT_INTENSITY, KC_AP_LED_SPEED, KC_TRNS, KC_TRNS,
-    MO(_FN2_LAYER), KC_TRNS, KC_UP, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PSCR, KC_HOME, KC_END, KC_TRNS,
-    KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PGUP, KC_PGDN, KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_INS, KC_DEL, KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, MO(_FN1_LAYER), MO(_FN2_LAYER), KC_TRNS
- ),
-   /*
-  * Mouse Layer _MSE_LAYER
+ [FN2] = LAYOUT_60_ansi( /* FN2 */
+    _______, KC_AP2_BT1, KC_AP2_BT2, KC_AP2_BT3, KC_AP2_BT4, _______, _______, _______, KC_AP_LED_OFF, KC_AP_LED_ON, _______, _______, _______, _______,
+    MO(FN2), _______,    KC_UP,      _______,    _______,    _______, _______, _______, _______, _______, KC_PSCR, KC_HOME, KC_END,  _______,
+    _______, KC_LEFT,    KC_DOWN,    KC_RGHT,    _______,    _______, _______, _______, _______, _______, KC_PGUP, KC_PGDN, _______,
+    _______,             _______,    _______,    _______,    _______, _______, _______, _______, _______, KC_INS,  KC_DEL,  _______,
+    _______, _______,    _______,                                     _______,                   _______, MO(FN1), MO(FN2), _______
+),
+  /*
+  * Mouse Layer MOUSE
   */
- [_MSE_LAYER] = LAYOUT( /* Base */
-    KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,
+ [MOUSE] = LAYOUT_60_ansi( /* MOUSE */
+    KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,
     _______, KC_WH_U, KC_WH_L, KC_MS_U, KC_WH_R,  _______, _______, _______, _______, _______, _______, _______, _______, _______,
-    KC_MS_BTN1, KC_WH_D, KC_MS_L, KC_MS_D, KC_MS_R, _______, _______, KC_RSFT, KC_RCTL, KC_RALT, KC_RGUI, KC_MS_BTN2, _______,
-    _______, KC_UNDO, KC_CUT,  KC_COPY, KC_PSTE, _______, _______, _______, _______, KC_INS, KC_DEL, _______,
-    KC_ACL2, KC_ACL1, KC_ACL0, _______, _______, _______, _______, _______
+    KC_MS_BTN2, KC_WH_D, KC_MS_L, KC_MS_D, KC_MS_R, _______, _______, KC_RSFT, KC_RCTL, KC_RALT, KC_RGUI, KC_MS_BTN2, _______,
+         _______, KC_UNDO, KC_CUT,  KC_COPY, KC_PSTE, _______, _______, _______, _______,   _______, _______,    _______,
+    KC_ACL2, KC_ACL1, KC_ACL0,                  KC_MS_BTN1,                                 _______, _______, _______, _______
 )
 };
 
@@ -128,17 +123,17 @@ enum anne_pro_layers {
 
 layer_state_t layer_state_set_user(layer_state_t state) {
     switch (get_highest_layer(state)) {
-        case _FN1_LAYER:
+        case FN1:
             // Set the leds to green
-            annepro2LedSetForegroundColor(0x00, 0xFF, 0x00);
+            ap2_led_set_foreground_color(0x00, 0xFF, 0x00);
             break;
-        case _FN2_LAYER:
+        case FN2:
             // Set the leds to blue
-            annepro2LedSetForegroundColor(0x00, 0x00, 0xFF);
+            ap2_led_set_foreground_color(0x00, 0x00, 0xFF);
             break;
         default:
             // Reset back to the current profile
-            annepro2LedResetForegroundColor();
+            ap2_led_reset_foreground_color();
             break;
     }
     return state;
@@ -149,16 +144,16 @@ layer_state_t layer_state_set_user(layer_state_t state) {
 bool led_update_user(led_t leds) {
     if (leds.caps_lock) {
         // Set the caps-lock to red
-        const annepro2Led_t color = {.p.red = 0xff, .p.green = 0x00, .p.blue = 0x00, .p.alpha = 0xff};
+        const ap2_led_t color = {.p.red = 0xff, .p.green = 0x00, .p.blue = 0x00, .p.alpha = 0xff};
 
-        annepro2LedMaskSetKey(2, 0, color);
+        ap2_led_mask_set_key(2, 0, color);
         /* NOTE: Instead of colouring the capslock only, you can change the whole
-           keyboard with annepro2LedSetForegroundColor */
+           keyboard with ap2_led_set_foreground_color */
     } else {
         // Reset the capslock if there is no layer active
-        if (!layer_state_is(_FN1_LAYER) && !layer_state_is(_FN2_LAYER)) {
-            const annepro2Led_t color = {.p.red = 0xff, .p.green = 0x00, .p.blue = 0x00, .p.alpha = 0x00};
-            annepro2LedMaskSetKey(2, 0, color);
+        if (!layer_state_is(FN1) && !layer_state_is(FN2)) {
+            const ap2_led_t color = {.p.red = 0xff, .p.green = 0x00, .p.blue = 0x00, .p.alpha = 0x00};
+            ap2_led_mask_set_key(2, 0, color);
         }
     }
 
